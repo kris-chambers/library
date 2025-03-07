49ac665849d2988a1e8b394fb0573880da669cc5
Kris Chambers <103455140+kris-chambers@users.noreply.github.com>
2025-03-07 16:57:11 -0500

    Book object, add new book function, normalize+reset css

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, numberOfPages, readStatus) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.readStatus = readStatus;
+  };
 
+function addBookToLibrary(title, author, numberOfPages, readStatus) {
+  const newBook = new Book(title, author, numberOfPages, readStatus)
+  myLibrary.push(newBook);
 };
 
-function addBookToLibrary() {
-
-};
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 300, "have read");
+addBookToLibrary("1984", "George Orwell", 328, "have not read");
+addBookToLibrary("To Kill A Mockingbird", "Harper Lee", 336, "have read");
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 114, "have read");
+addBookToLibrary("Of Mice and Men", "John Steinbeck", 107, "have not read");
