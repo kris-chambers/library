529c61abec1cc72ca7fd50f32eb2373ccf79b60b
Kris Chambers <103455140+kris-chambers@users.noreply.github.com>
2025-03-07 18:36:52 -0500

    Will dynamically display books

--- js/script.js
+++ js/script.js
@@ -13,8 +13,31 @@ function addBookToLibrary(title, author, numberOfPages, readStatus) {
   myLibrary.push(newBook);
 };
 
+function displayBooks(arrayOfBooks) {
+  const library = document.querySelector("#library");
+  
+  
+  for (let i = 0; i < arrayOfBooks.length; i++) {
+    const newDiv = document.createElement("div");
+    const newSpan = document.createElement("span");
+    
+    newSpan.innerHTML = `Title: ${arrayOfBooks[i].title}<br>
+                      Author: ${arrayOfBooks[i].author}<br>
+                      Number of Page: ${arrayOfBooks[i].numberOfPages}<br>
+                      Read Status: ${arrayOfBooks[i].readStatus}`;
+
+    library.appendChild(newDiv);
+    newDiv.classList.add("bookCard");
+    newDiv.appendChild(newSpan);
+    newSpan.classList.add("bookInfo");
+  }
+}
+
+
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 300, "have read");
 addBookToLibrary("1984", "George Orwell", 328, "have not read");
 addBookToLibrary("To Kill A Mockingbird", "Harper Lee", 336, "have read");
 addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 114, "have read");
 addBookToLibrary("Of Mice and Men", "John Steinbeck", 107, "have not read");
+
+displayBooks(myLibrary);
