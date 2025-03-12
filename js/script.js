df678bb47d8de273ba2533949ec76c3bd1168cce
Kris Chambers <103455140+kris-chambers@users.noreply.github.com>
2025-03-12 15:43:27 -0400

    Added delete button. Still working on it

--- js/script.js
+++ js/script.js
@@ -6,17 +6,13 @@ function Book(title, author, numberOfPages, readStatus) {
   this.author = author;
   this.numberOfPages = numberOfPages;
   this.readStatus = readStatus;
-  };
-
-// function addBookToLibrary(title, author, numberOfPages, readStatus) {
-//   const newBook = new Book(title, author, numberOfPages, readStatus)
-//   myLibrary.push(newBook);
-// };
+};
 
 function addBookToLibrary(book) {
   const newBook = new Book(book.title, book.author, book.numberOfPages, book.readStatus);
   myLibrary.push(newBook);
-}
+  console.log("Book added to library:", book.title, "My library size:", myLibrary.length)
+};
 
 function displayBooks(arrayOfBooks) {
   const library = document.querySelector("#library");
@@ -24,6 +20,8 @@ function displayBooks(arrayOfBooks) {
   for (let i = 0; i < arrayOfBooks.length; i++) {
     const newDiv = document.createElement("div");
     const newSpan = document.createElement("span");
+    const newButton = document.createElement("button");
+    const deleteText = document.createTextNode("Delete");
     
     newSpan.innerHTML = `Title: ${arrayOfBooks[i].title}<br>
                       Author: ${arrayOfBooks[i].author}<br>
@@ -32,20 +30,39 @@ function displayBooks(arrayOfBooks) {
 
     library.appendChild(newDiv);
     newDiv.classList.add("bookCard");
+    newDiv.dataset.uuid = `${arrayOfBooks[i].id}`;
     newDiv.appendChild(newSpan);
     newSpan.classList.add("bookInfo");
-  }
-}
+    newSpan.appendChild(newButton);
+    newButton.classList.add("deleteButton");
+    newButton.id = `${arrayOfBooks[i].id}`;
+    newButton.appendChild(deleteText);
+    newButton.dataset.uuid = `${arrayOfBooks[i].id}`;
+    // newButton.addEventListener("click", (e) => {
+    //   const bookId = e.target.id;
+    //   console.log('Clicked button:', bookId)
+
+    //   const bookToRemove = myLibrary.findIndex(myLibrary => myLibrary.id == bookId);
+    //   if (bookToRemove !== -1) {
+    //     myLibrary.splice(bookToRemove, 1);
+    //     console.log(myLibrary);
+    //   };
+    // });
+  };
+  console.log("Displayed books.")
+};
 
 function clearBooks() {
   const library = document.querySelector("#library");
   library.innerHTML = '';
-}
+  console.log("Screen cleared.")
+};
 
 function clearForm() {
   form = document.querySelector("#addBookForm");
   form.reset();
-}
+  console.log("Form cleared.")
+};
 
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("#newBook");
@@ -58,9 +75,8 @@ function addInput() {
   const numberOfPagesInput = document.querySelector("#numberOfPages");
   const readStatusInput = document.querySelector("#readStatus");
 
-  console.log(`${titleInput.value}, ${authorInput.value}, ${numberOfPagesInput.value}, ${readStatusInput.value}`)
   return { title: titleInput.value, author: authorInput.value, numberOfPages: numberOfPagesInput.value, readStatus: readStatusInput.value };
-}
+};
 
 showButton.addEventListener("click", () => {
   dialog.showModal();
@@ -68,7 +84,7 @@ showButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
   dialog.close();
-})
+});
 
 addButton.addEventListener("click", (event) => {  
   event.preventDefault();
@@ -77,45 +93,44 @@ addButton.addEventListener("click", (event) => {
   dialog.close();
   clearForm();
   clearBooks();
-  displayBooks(myLibrary)
+  displayBooks(myLibrary);
 });
 
 const theHobbit = {
   title: "The Hobbit",
   author: "J.R.R. Tolkien",
   numberOfPages: 300,
-  readStatus: "have read"
-}
+  readStatus: "have read",
+};
 
 const _1984 = {
   title: "1984",
   author: "George Orwell",
   numberOfPages: 328,
-  readStatus: "have not read"
-}
+  readStatus: "have not read",
+};
 
 const toKillAMockingbird = {
   title: "To Kill A Mockingbird",
   author: "Harper Lee",
   numberOfPages: 336,
-  readStatus: "have read"
-}
+  readStatus: "have read",
+};
 
 const theGreatGatsby = {
   title: "The Great Gatsby",
   author: "F. Scott Fitzgerald",
   numberOfPages: 114,
-  readStatus: "have read"
-}
+  readStatus: "have read",
+};
 
 const ofMiceAndMen = {
   title: "Of Mice And Men",
   author: "John Steinbeck",
   numberOfPages: 107,
-  readStatus: "have not read"
-}
+  readStatus: "have not read",
+};
 
-// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 300, "have read");
 addBookToLibrary(theHobbit);
 addBookToLibrary(_1984);
 addBookToLibrary(toKillAMockingbird);
@@ -123,3 +138,23 @@ addBookToLibrary(theGreatGatsby);
 addBookToLibrary(ofMiceAndMen);
 
 displayBooks(myLibrary);
+
+const deleteButtons = document.querySelectorAll(".deleteButton");
+
+deleteButtons.forEach(button => {
+  button.addEventListener("click", (e) => {
+    // e.preventDefault();
+    // e.stopPropagation();
+    
+    const bookId = e.target.id;
+    console.log('Removed book:', bookId)
+
+    const bookToRemove = myLibrary.findIndex(myLibrary => myLibrary.id == bookId);
+    if (bookToRemove !== -1) {
+      myLibrary.splice(bookToRemove, 1);
+    }
+    console.log("Added delete button event listeners.");
+    clearBooks();
+    displayBooks(myLibrary);
+    });
+});
